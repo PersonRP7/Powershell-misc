cb09f8f96cda58037b264b0623e9bbb92d9e6c2a
Person <zemlja-trp@yandex.com>
2020-08-20 20:28:56 +0200

    Added

--- Profile/Profile.ps1
+++ Profile/Profile.ps1
@@ -10,8 +10,27 @@ Function drm {docker rm (docker ps -a -q)}
 #Alias for the Windows Subsystem for Linux text editor (pico).
 Function pico {wsl pico}
 
+# Lists the contents of the Modules directories
 Function list_modules {dir C:\Users\$env:USERNAME\Documents\WindowsPowerShell\Modules}
 
+# Copies the active Profile.ps1 file to its git tracked directory.
 Function update_profile {
     Copy-Item C:\Users\$env:USERNAME\Documents\WindowsPowerShell\Profile.ps1 C:\Users\$env:USERNAME\Documents\powershell\Profile
+}
+
+# Reads the source code of a module.
+Function read_module($module){
+    cat C:\Users\$env:USERNAME\Documents\WindowsPowerShell\Modules\$module\$module.psm1
+}
+
+# Updates the git tracked powershell directory and commits to the external repository.
+Function update_git
+{
+    param(
+        [string]$comment=$(throw "A comment must be added for git commit.")
+    )
+    Set-Location C:\Users\$env:USERNAME\Documents\powershell
+    git add -A
+    git commit -m "$comment"
+    git push -u origin master
 }
